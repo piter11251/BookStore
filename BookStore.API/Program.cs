c58d65112d4b349a7f9f64e23aa54482f180321f
piter11251 <duszeq13@gmail.com>
2025-04-06 18:36:42 +0200

    Created entities and BookStoreDbContext

--- BookStore.API/Program.cs
+++ BookStore.API/Program.cs
@@ -1,7 +1,15 @@
+using BookStore.Infrastructure.Context;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer; // Add this using directive
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<BookStoreDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllers();
 
 var app = builder.Build();
