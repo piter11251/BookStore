65297153370fe881cbeb2ec649984ff891c318c2
piter11251 <duszeq13@gmail.com>
2025-04-08 15:36:09 +0200

    api done

---
+++ BookStore.API/DTOs/Category/CategoryResultDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BookStore.API.DTOs.Category
+{
+    public class CategoryResultDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- BookStore.API/Program.cs
+++ BookStore.API/Program.cs
@@ -1,7 +1,10 @@
 using BookStore.Infrastructure.Context;
+using Microsoft.OpenApi.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.SqlServer; // Add this using directive
+using Microsoft.EntityFrameworkCore.SqlServer;
+using BookStore.API.Configuration;
+using Microsoft.Extensions.DependencyInjection; // Add this using directive
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,12 +13,26 @@ builder.Services.AddDbContext<BookStoreDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.ResolveDependencies();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Title = "BookStore API",
+        Version = "v1"
+    });
+});
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+});
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
